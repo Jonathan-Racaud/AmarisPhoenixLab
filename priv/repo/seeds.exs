4b9e7ed657bfe89e69e8c3360663c2b4597d26b4
Jonathan Racaud <jonathan.racaud@outlook.com>
2020-09-08 10:03:09 +0200

    Added Data to be seeded

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,46 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias AmarisPhoenixLab.Accounts
+alias AmarisPhoenixLab.CMS
+
+Accounts.create_user(%{
+  name: "Admin",
+  email: "admin@example.com"
+});
+
+CMS.create_category(%{
+  name: "Web"
+})
+
+CMS.create_category(%{
+  name: "Mobile"
+})
+
+CMS.create_category(%{
+  name: "Desktop"
+})
+
+CMS.create_category(%{
+  name: "Techno study"
+})
+
+CMS.create_category(%{
+  name: "Market study"
+})
+
+CMS.create_material_type(%{
+  name: "Document"
+})
+
+CMS.create_material_type(%{
+  name: "Image"
+})
+
+CMS.create_material_type(%{
+  name: "Video"
+})
+
+CMS.create_material_type(%{
+  name: "Url"
+})
