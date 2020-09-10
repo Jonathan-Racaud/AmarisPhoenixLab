486d55339790a234a8290fa58c17c491dfa9ea1b
Jonathan Racaud <jonathan.racaud@outlook.com>
2020-09-10 11:08:39 +0200

    Edit links are now displayed only when user is admin

--- lib/amaris_phoenix_lab_web/live/project_live/show.ex
+++ lib/amaris_phoenix_lab_web/live/project_live/show.ex
@@ -2,10 +2,13 @@ defmodule AmarisPhoenixLabWeb.ProjectLive.Show do
   use AmarisPhoenixLabWeb, :live_view
 
   alias AmarisPhoenixLab.CMS
+  alias AmarisPhoenixLab.Users
+  alias AmarisPhoenixLabWeb.Credentials
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, socket}
+  def mount(_params, session, socket) do
+    current_user = Credentials.get_user(socket, session)
+    {:ok, assign(socket, current_user: current_user)}
   end
 
   @impl true
